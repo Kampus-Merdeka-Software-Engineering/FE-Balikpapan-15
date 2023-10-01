44e1cf1662dd9399a04eb3f55cb7431b0336c377
danang507 <danangwijayanto507@gmail.com>
2023-10-01 22:01:16 +0700

    add button back to top

--- js/script.js
+++ js/script.js
@@ -5,3 +5,12 @@ function toggleMenu(){
     icon.classList.toggle("open");
 
 }
+
+const toTop = document.querySelector(".to-top");
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+})
