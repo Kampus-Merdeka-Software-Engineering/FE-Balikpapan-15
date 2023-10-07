07d17a1fc692ae4124fa028ff6be1e5d72cc8e52
danang507 <danangwijayanto507@gmail.com>
2023-10-08 00:52:23 +0700

    final revisi

--- js/script.js
+++ js/script.js
@@ -16,3 +16,63 @@ window.addEventListener("scroll", () => {
     toTop.classList.remove("active");
   }
 })
+
+//memanggil data untuk course data analytics dari database
+document.addEventListener('DOMContentLoaded', () => {
+  fetch('https://be-balikpapan-15-production.up.railway.app/materi/category/1')
+    .then(response => response.json())
+    .then(data => {
+      const videoList = document.getElementById('video-list');
+      data.forEach(materi => {
+        const listItem = document.createElement('li');
+        let link = materi.link
+        link = link.replace('https://youtu.be/','https://www.youtube.com/embed/')
+         listItem.innerHTML = `
+           <h1>${materi.judul}</h1>
+           <iframe width="560" height="400" src="${link}" frameborder="0" allowfullscreen></iframe>
+           <p>${materi.deskripsi}</p>
+         `;
+         videoList.appendChild(listItem);
+      });
+    });
+});
+
+//memanggil data untuk course frondend dari database
+document.addEventListener('DOMContentLoaded', () => {
+  fetch('https://be-balikpapan-15-production.up.railway.app/materi/category/2')
+    .then(response => response.json())
+    .then(data => {
+      const videoList = document.getElementById('video-list-frondend');
+      data.forEach(materi => {
+        const listItem = document.createElement('li');
+        let link = materi.link
+        link = link.replace('https://youtu.be/','https://www.youtube.com/embed/')
+         listItem.innerHTML = `
+           <h1>${materi.judul}</h1>
+           <iframe width="560" height="400" src="${link}" frameborder="0" allowfullscreen></iframe>
+           <p>${materi.deskripsi}</p>
+         `;
+         videoList.appendChild(listItem);
+      });
+    });
+});
+
+//memanggil data untuk course backend dari database
+document.addEventListener('DOMContentLoaded', () => {
+  fetch('https://be-balikpapan-15-production.up.railway.app/materi/category/3')
+    .then(response => response.json())
+    .then(data => {
+      const videoList = document.getElementById('video-list-backend');
+      data.forEach(materi => {
+        const listItem = document.createElement('li');
+        let link = materi.link
+        link = link.replace('https://youtu.be/','https://www.youtube.com/embed/')
+         listItem.innerHTML = `
+           <h1>${materi.judul}</h1>
+           <iframe width="560" height="400" src="${link}" frameborder="0" allowfullscreen></iframe>
+           <p>${materi.deskripsi}</p>
+         `;
+         videoList.appendChild(listItem);
+      });
+    });
+});
